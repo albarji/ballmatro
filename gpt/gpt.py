c7d263e623b0c69a20bde1a2d94d78627a9d7b07
albarji <albarjip@gmail.com>
2025-08-12 11:08:58 +0200

    feat: force CoT end if generation takes too long

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -15,6 +15,15 @@ LOGGER = logging.getLogger(__name__)
 
 README_PATH = "README.md"
 
+THINKING_MODELS_FINISHERS = {
+    "Qwen/Qwen3": "</think>",
+    "openai/gpt-oss": "assistantfinal"
+}
+
+ALTERNATIVE_CHAT_TEMPLATES = {
+    "Qwen/Qwen3": "gpt/chat_templates/qwen3",
+}
+
 def gpt_attempt_ballmatro_dataset(dataset: list[dict], model: str = "gpt-4o") -> list[str]:
     """Use a GPT model to attempt to solve a Ballmatro dataset.
 
@@ -37,7 +46,7 @@ def gpt_attempt_ballmatro_dataset(dataset: list[dict], model: str = "gpt-4o") ->
         responses.append(content)
     return ScoreDataset(dataset, responses)
 
-def hf_attempt_ballmatro_dataset(dataset: list[dict], model_name: str) -> list[str]:
+def hf_attempt_ballmatro_dataset(dataset: list[dict], model_name: str, max_new_tokens: int = 16384) -> list[str]:
     """Use a Hugging Face model to attempt to solve a Ballmatro dataset.
 
     Returns a list of responses from the model for each item in the dataset.
@@ -48,11 +57,16 @@ def hf_attempt_ballmatro_dataset(dataset: list[dict], model_name: str) -> list[s
     )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+    # Load alternative chat template if it exists
+    if model_name in ALTERNATIVE_CHAT_TEMPLATES:
+        with open(ALTERNATIVE_CHAT_TEMPLATES[model_name], "r") as f:
+            tokenizer.template = f.read()
+
     generation_pipeline = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_new_tokens=4096,
+        max_new_tokens=max_new_tokens,
     )
 
     system_prompt = build_system_prompt()
@@ -63,16 +77,45 @@ def hf_attempt_ballmatro_dataset(dataset: list[dict], model_name: str) -> list[s
             {"role": "user", "content": data["input"]}
         ]
         result = generation_pipeline(messages)[0]["generated_text"][-1]["content"]
-        # Remove <think></think> section from the result, if present (Qwen 3 models)
-        result = re.sub(r"<think>.*?</think>", "", result, flags=re.DOTALL).strip()
-        # Remove everything up to </think> mark from the result, if present (Qwen 3 Thinking models)
-        result = re.sub(r"^.*?</think>", "", result, flags=re.DOTALL).strip()
-        # Remove analysis-assistantfinal section from the result, if present (gpt-oss models)
-        result = re.sub(r"^analysis.*?assistantfinal", "", result, flags=re.DOTALL).strip()
+        # If thinking model but chain of thought is unfinished, continue generation forcing final response
+        if _is_thinking_model(model_name) and not _thinking_finished(result):
+            LOGGER.warning(f"Model {model_name} did not finish thinking. Forcing generation to end.")
+            messages.append({"role": "assistant", "content": result + "\n" + _thinking_model_finisher(model_name)})
+            result = generation_pipeline(messages)[0]["generated_text"][-1]["content"]
+        # result = _generate_response(model, tokenizer, system_prompt, data["input"], max_new_tokens)
+        if _is_thinking_model(model_name):
+            result = _remove_chain_of_thought(result)
         LOGGER.info(f"({i+1}/{len(dataset)}): {data['input']} -> {result}")
         # Append the response content to the list
         responses.append(result)
     return ScoreDataset(dataset, responses)
+
+def _is_thinking_model(model_name: str) -> bool:
+    """Tries to identify by its name if a Hugging Face model is a thinking model. Returns False for unknown models"""
+    return any(model_name.startswith(prefix) for prefix in THINKING_MODELS_FINISHERS.keys())
+
+def _thinking_model_finisher(model_name: str) -> str:
+    """Return the string that marks the end of a chain of thought for the given model.
+
+    Return empty string if the model is not a known thinking model"""
+    for key, value in THINKING_MODELS_FINISHERS.items():
+        if model_name.startswith(key):
+            return value
+    return ""
+
+def _thinking_finished(model_output: str) -> bool:
+    """Check if the thinking process has finished based on the model output."""
+    return any(finisher in model_output for finisher in THINKING_MODELS_FINISHERS.values())
+
+def _remove_chain_of_thought(result: str) -> str:
+    """Removes the chaing of thought part from the output, returning only the final LLM response"""
+    # Remove <think></think> section from the result, if present (Qwen 3 models)
+    result = re.sub(r"<think>.*?</think>", "", result, flags=re.DOTALL).strip()
+    # Remove everything up to </think> mark from the result, if present (Qwen 3 Thinking models)
+    result = re.sub(r"^.*?</think>", "", result, flags=re.DOTALL).strip()
+    # Remove analysis-assistantfinal section from the result, if present (gpt-oss models)
+    result = re.sub(r"^analysis.*?assistantfinal", "", result, flags=re.DOTALL).strip()
+    return result
 
 def build_system_prompt() -> str:
     """Build the system prompt for the GPT model, making use of the README file."""
--- gpt/tests/gpt_test.py
+++ gpt/tests/gpt_test.py
@@ -1,9 +1,38 @@
 """Tests for the GPT module."""
 
-from gpt.gpt import build_system_prompt
+from gpt.gpt import _is_thinking_model, _remove_chain_of_thought, _thinking_finished, build_system_prompt
 
 def test_build_system_prompt():
     """Test the build_system_prompt function."""
     prompt = build_system_prompt()
     assert prompt is not None
     assert "In each game of BaLLMatro you will get a list of cards" in prompt
+
+def test_is_thinking_model():
+    """Test the _is_thinking_model function."""
+    assert _is_thinking_model("Qwen/Qwen3-4B-Instruct-2507") is True
+    assert _is_thinking_model("Qwen/Qwen3-4B-Thinking-2507") is True
+    assert _is_thinking_model("openai/gpt-oss-20b") is True
+    assert _is_thinking_model("openai/gpt-oss-120b") is True
+    assert _is_thinking_model("Qwen/Qwen2.5-0.5B") is False
+
+def test_thinking_finished():
+    """Test the _thinking_finished function."""
+    assert _thinking_finished("<think>We have two apples and get three, so the result must be five</think>5") is True
+    assert _thinking_finished("We have two apples and get three, so the result must be five</think>5") is True
+    assert _thinking_finished("We have two apples and get three, so the result must be fiveassistantfinal5") is True
+    assert _thinking_finished("We have two apples and get three, so the result must") is False
+    assert _thinking_finished("5") is False
+    assert _thinking_finished("This is a test without any thinking markers") is False
+    assert _thinking_finished("</think>") is True
+    assert _thinking_finished("assistantfinal") is True
+
+def test_remove_chain_of_thought():
+    """Test the _remove_chain_of_thought function."""
+    assert _remove_chain_of_thought("<think>We have two apples and get three, so the result must be five</think>5") == "5"
+    assert _remove_chain_of_thought("We have two apples and get three, so the result must be five</think>5") == "5"
+    assert _remove_chain_of_thought("analysisWe have two apples and get three, so the result must be fiveassistantfinal5") == "5"
+    assert _remove_chain_of_thought("We have two apples and get three, so the result must") == "We have two apples and get three, so the result must"
+    assert _remove_chain_of_thought("5") == "5"
+    assert _remove_chain_of_thought("This is a test without any thinking markers") == "This is a test without any thinking markers"
+    assert _remove_chain_of_thought("</think>") == ""
